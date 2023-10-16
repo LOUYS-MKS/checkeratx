7fcefed9af5ab1758e0a53be6b46e101df7d0060
LOUYS <89886859+LOUYS-MKS@users.noreply.github.com>
2023-10-16 13:04:42 -0300

    Add files via upload

--- instcheck.sh
+++ instcheck.sh
@@ -1,51 +1,55 @@
 #!/bin/bash
 
 check_installed() {
-    if [ -f "/usr/lib/chuser/atx.py" ]; then
+    if [ -f "/usr/lib/chkuser/atx.py" ]; then
         return 0  # Já instalado
     else
         return 1  # Não instalado
     fi
 }
+
 clear
+dialog --infobox "Configurando o fuso horário..." 5 40
 echo "America/Sao_Paulo" > /etc/timezone
 ln -fs /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime > /dev/null 2>&1
 dpkg-reconfigure --frontend noninteractive tzdata > /dev/null 2>&1
-clear
-echo -e "\E[44;1;37m    INSTALAR CHECKUSER ATX TUNNEL     \E[0m" 
-echo ""
-echo -e "                 \033[1;31mBy @LOUYSZHX\033[1;36m"
-echo ""
-echo -ne "\n\033[1;32mDE UM ENTER PARA \033[1;33mCONTINUAR...\033[1;37m: "; read -r
-clear
-echo -e "\n\033[1;36mINICIANDO INSTALAÇÃO \033[1;33mAGUARDE..."
-apt-get install figlet -y > /dev/null 2>&1
-pip3 install flask > /dev/null 2>&1
-rm /bin/chuser > /dev/null 2>&1
-sleep 5
-cd /bin || exit
-wget https://raw.githubusercontent.com/LOUYS-MKS/checkeratx/main/chuser > /dev/null 2>&1
-wget https://raw.githubusercontent.com/LOUYS-MKS/checkeratx/main/userscheck > /dev/null 2>&1
-chmod 777 chuser > /dev/null 2>&1
-chmod 777 userscheck > /dev/null 2>&1
 
 clear
+dialog --msgbox "INSTALAR CHECKUSER ATX TUNNEL" 7 40
+dialog --msgbox "By @LOUYSZHX" 5 40
+
+dialog --yesno "Pressione Sim para continuar ou Não para sair." 7 40
+response=$?
+if [ $response -eq 1 ]; then
+    clear
+    exit 1
+fi
+
+clear
+dialog --infobox "Iniciando instalação. Aguarde..." 5 40
+apt-get install figlet -y > /dev/null 2>&1
+pip3 install flask > /dev/null 2>&1
+rm /bin/chkuser > /dev/null 2>&1
+sleep 5
+cd /bin || exit
+wget https://raw.githubusercontent.com/LOUYS-MKS/checkeratx/main/chkuser > /dev/null 2>&1
+wget https://raw.githubusercontent.com/LOUYS-MKS/checkeratx/main/userschk > /dev/null 2>&1
+chmod 777 chkuser > /dev/null 2>&1
+chmod 777 userschk > /dev/null 2>&1
+
+clear
+dialog --infobox "Concluindo a instalação..." 5 40
 mkdir /usr/lib/checkuser > /dev/null 2>&1
 cd /usr/lib/checkuser || exit
 rm atx.py > /dev/null 2>&1
 wget https://raw.githubusercontent.com/LOUYS-MKS/checkeratx/main/atx.py > /dev/null 2>&1
 chmod 777 atx.py > /dev/null 2>&1
+
 clear
-echo -e "        \033[1;33m • \033[1;32mINSTALAÇÃO CONCLUÍDA\033[1;33m • \033[0m"
-sleep 2
+dialog --msgbox "INSTALAÇÃO CONCLUÍDA" 5 40
+
 clear
-echo ""
-echo -e "\033[1;31m \033[1;33mCOMANDO PRINCIPAL: \033[1;32mchuser\033[0m"
-echo ""
-echo -e "\033[1;33m MAIS INFORMAÇÕES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@LOUYSZHX\033[0m"
+dialog --msgbox "Comando principal: chkuser" 5 40
+dialog --msgbox "Mais informações (Telegram): @LOUYSZHX" 5 40
+
 cat /dev/null > ~/.bash_history && history -c
-exit
-
-
-
-
