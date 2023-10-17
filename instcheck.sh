4486423ec6776ef10b28cc6eeaf1e66264a74d28
LOUYS <89886859+LOUYS-MKS@users.noreply.github.com>
2023-10-17 18:39:00 -0300

    Add files via upload

--- instcheck.sh
+++ instcheck.sh
@@ -5,7 +5,6 @@ sudo apt update && sudo apt upgrade
 sudo apt-get install dialog
 sleep 7
 echo "Aguarde..."
-
 check_installed() {
     if [ -f "/usr/lib/chuser/atx.py" ]; then
         return 0  # Já instalado
@@ -13,8 +12,13 @@ check_installed() {
         return 1  # Não instalado
     fi
 }
-
 clear
+
+
+
+
+
+
 dialog --infobox "Configurando o fuso horário..." 5 40
 echo "America/Sao_Paulo" > /etc/timezone
 ln -fs /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime > /dev/null 2>&1
@@ -56,6 +60,5 @@ dialog --msgbox "INSTALAÇÃO CONCLUÍDA" 5 40
 
 clear
 dialog --msgbox "Comando principal: chuser" 5 40
-dialog --msgbox "Mais informações (Telegram): @LOUYSZHX" 5 40
 
 cat /dev/null > ~/.bash_history && history -c
